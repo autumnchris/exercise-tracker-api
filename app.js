7f390d90006553f111464ce2fe8b8a5d8462c09b
Autumn <autumncbullard@gmail.com>
2018-11-08 16:31:36 -0500

    Set up body-parser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 mongoose.connect(process.env.MONGO_URI);
 
