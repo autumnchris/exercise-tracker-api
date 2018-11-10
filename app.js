be8f4601162560e7d391022de06244d436a4fe51
Autumn <autumncbullard@gmail.com>
2018-11-10 13:53:41 -0500

    Build POST request for add exercise form

--- app.js
+++ app.js
@@ -34,6 +34,41 @@ app.post('/api/exercise/new-user', (req, res) => {
   });
 });
 
+app.post('/api/exercise/add', (req, res) => {
+  let exercise;
+
+  if (moment(req.body.date, 'YYYY-M-D', true).isValid() || req.body.date === '') {
+    exercise = new Exercise({
+      userId: req.body.userId,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: new Date(req.body.date).getTime() || new Date().getTime()
+    });
+    exercise.save().then(exerciseData => {
+      User.findByIdAndUpdate(req.body.userId, {$push: {log: exerciseData}, $inc: {count: 1}}).then(userData => {
+        res.json({
+          _id: userData._id,
+          username: userData.username,
+          exercise: {
+            description: exerciseData.description,
+            duration: exerciseData.duration,
+            date: new Date(exerciseData.date).toDateString()
+          }
+        });
+      }).catch(error => {
+        res.json({ error });
+      });
+    }).catch(error => {
+      res.json({ error });
+    });
+  }
+  else {
+    res.json({
+      error: 'Invalid date. Please use YYYY-MM-DD format.'
+    });
+  }
+});
+
 app.get('/api/exercise/users', (req, res) => {
   User.find({}, '_id username').then(userData => {
     res.json(userData);
