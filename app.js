32e8f5e19049e7e95e3aa79aa9b72b42abd939fd
Autumn <autumncbullard@gmail.com>
2018-11-14 10:12:45 -0500

    Build GET request that shows and filters a user's exercise log

--- app.js
+++ app.js
@@ -77,6 +77,56 @@ app.get('/api/exercise/users', (req, res) => {
   });
 });
 
+app.get('/api/exercise/log/:userid', (req, res) => {
+  const limit = parseInt(req.query.limit);
+  let from = req.query.from;
+  let to = req.query.to;
+  let log;
+
+  if ((moment(from, 'YYYY-MM-DD', true).isValid() && moment(to, 'YYYY-MM-DD', true).isValid()) || !from && !to) {
+    User.findById(req.params.userid, '_id username log count').populate({
+      path: 'log',
+      select: `description duration date -_id`,
+      options: {
+        sort: { date: 'desc' }
+      }
+    }).then(userData => {
+      log = userData.log;
+
+      if (from && to) {
+        log = log.filter(exercise => moment(exercise.date).isBetween(from, to, null, []));
+      }
+
+      if (limit) {
+        log = log.slice(0, limit);
+      }
+
+      log = log.reduce((acc, exercise) => {
+        acc.push({
+          description: exercise.description,
+          duration: exercise.duration,
+          date: new Date(exercise.date).toISOString().split('T')[0]
+        });
+        return acc;
+      }, []);
+
+      res.json({
+        _id: userData._id,
+        username: userData.username,
+        log,
+        count: userData.count
+      });
+    }).catch(error => {
+      res.json({ error });
+    });
+  }
+  else {
+    res.json({
+      error: 'Invalid date range.'
+    });
+  }
+});
+
 app.use((req, res) => {
   res.sendFile(`${__dirname}/views/404.html`, 404);
 });
