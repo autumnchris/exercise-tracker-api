38140cb48c2167d3009d38d8d789b6e4d7032c9f
autumnchris <autumncbullard@gmail.com>
2020-01-12 18:06:54 -0500

    Add variables for routing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
 
 const app = express();
 const port = process.env.PORT || 3000;
